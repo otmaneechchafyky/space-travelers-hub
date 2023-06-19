b9706614dbd51418a9b35f048ab775256100817c
Reggeditt <tkodjosarso@gmail.com>
2023-06-19 21:11:52 +0000

    setup store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import Layout from './routes/Layout';
 import Home from './routes/Home';
 import Profile from './routes/Profile';
@@ -10,15 +12,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="Rockets" element={<Rockets />} />
-          <Route path="Missions" element={<Missions />} />
-          <Route path="*" element={<NotMatch />} />
-        </Route>
-      </Routes>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="Rockets" element={<Rockets />} />
+            <Route path="Missions" element={<Missions />} />
+            <Route path="*" element={<NotMatch />} />
+          </Route>
+        </Routes>
+      </Provider>
     </div>
   );
 }
