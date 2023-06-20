6cf471ee45919de2fe7bd4ea8ff8ec1a8a2fc6e3
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-06-20 10:00:18 +0100

    Adding Navbar and set up Redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './routes/Layout';
-import Home from './routes/Home';
 import Profile from './routes/Profile';
 import Rockets from './routes/Rockets';
 import Missions from './routes/Missions';
@@ -12,9 +11,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
           <Route path="Rockets" element={<Rockets />} />
+          <Route path="profile" element={<Profile />} />
           <Route path="Missions" element={<Missions />} />
           <Route path="*" element={<NotMatch />} />
         </Route>
