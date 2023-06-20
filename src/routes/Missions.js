92773bf0592046b8bd357412084d1fc0ad3846d4
Reggeditt <tkodjosarso@gmail.com>
2023-06-20 16:02:50 +0000

    fixed #2; display jouned missions on profile page

--- src/routes/Missions.js
+++ src/routes/Missions.js
@@ -1,15 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import MissionsEntry from '../components/missions/missionEntry';
-import { fetchMissions } from '../redux/missions/missionsSlice';
 
 const Missions = () => {
-  const dispatch = useDispatch();
   const missions = useSelector((store) => store.missions.missions);
 
-  useEffect(() => {
-    dispatch(fetchMissions());
-  }, [dispatch]);
   return (
     <div>
       <h2>Missions</h2>
