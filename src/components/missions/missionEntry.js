1dec9f5fe926da9972e39629000c0b266c45a195
Tetteh Kodjo-Sarso <Tkodjosarso@gmail.com>
2023-06-20 16:02:24 +0000

    Update missionEntry.js

--- src/components/missions/missionEntry.js
+++ src/components/missions/missionEntry.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
