af402910a76de83bcc3dfb8bc5f5316d7e8f742d
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-06-21 18:33:42 +0100

    Left Rockets state to App component and resolve Conficts

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from './redux/rockets/rocketsSlice';
+import { fetchMissions } from './redux/missions/missionsSlice';
 import Layout from './routes/Layout';
 import Profile from './routes/Profile';
 import Rockets from './routes/Rockets';
 import Missions from './routes/Missions';
 import NotMatch from './routes/NotMatch';
 import './App.css';
-import { fetchMissions } from './redux/missions/missionsSlice';
 
 function App() {
   const rocketsList = useSelector((state) => state.rockets.rocketsList);
@@ -25,7 +25,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="Rockets" element={<Rockets rocketsList={rocketsList} />} />
+          <Route
+            path="Rockets"
+            element={<Rockets rocketsList={rocketsList} />}
+          />
           <Route path="profile" element={<Profile />} />
           <Route path="Missions" element={<Missions />} />
           <Route path="*" element={<NotMatch />} />
