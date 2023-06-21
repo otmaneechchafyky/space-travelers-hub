3009103cce004a99fa3eb37b24f58b1e833b93a6
Reggeditt <tkodjosarso@gmail.com>
2023-06-21 12:10:17 +0000

    add tailwind

---
+++ src/routes/SignUp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp = () => (
+  <div>SignUp here</div>
+);
+
+export default SignUp;
---
+++ src/routes/signIn.js
@@ -0,0 +1,5 @@
+const SignIn = () => (
+  <div>Sign in here</div>
+);
+
+export default SignIn;
